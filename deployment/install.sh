2e9a0b866cb55630e66b1e18a8c2720bf1cc4324
PSNAppZ <thepsnarayanan@gmail.com>
2024-04-23 19:44:13 +0530

    Spar Mapper Deployment Fix

--- deployment/install.sh
+++ deployment/install.sh
@@ -4,11 +4,11 @@ export SANDBOX_HOSTNAME=${SANDBOX_HOSTNAME:-dev.openg2p.net}
 export SPAR_HOSTNAME=${SPAR_HOSTNAME:-spar.$SANDBOX_HOSTNAME}
 
 COPY_UTIL=./copy_cm_func.sh
-NS=spar
+NS=spar-heml-test
 
 echo "Create $NS namespace"
 kubectl create ns $NS
 
 $COPY_UTIL secret postgres-postgresql postgres $NS
 
-helm -n $NS upgrade --install openg2p ../ -f ../deployment/values.yaml --set global.hostname=$SPAR_HOSTNAME --wait $@
+helm -n $NS upgrade --install spar ../charts/spar-mapper-api -f values.yaml --set global.hostname=$SPAR_HOSTNAME --wait $@
